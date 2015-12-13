21615213abaa9a87ad59a18e94fb701f3230a3ca
BurakAy <burak@burak-aydemir.com>
2015-12-13 11:11:38 -0600

    added second prompt for columns

--- javascript/grid.js
+++ javascript/grid.js
@@ -1,7 +1,7 @@
 // function builds a grid
-function createGrid(x) {
+function createGrid(x, z) {
 	for (var rows = 0; rows < x; rows++) {
-		for (var columns = 0; columns < x; columns++) {
+		for (var columns = 0; columns < z; columns++) {
 			$('#container').append('<div class="grid"></div>');
 		};
 	};
@@ -17,13 +17,14 @@ function clearGrid() {
 // prompt user to select new grid dimension
 function refreshGrid() {
 	var z = prompt("Enter new grid dimension");
+	var y = prompt("Enter new grid column");
 	clearGrid();
-	createGrid(z);
+	createGrid(z, y);
 }
 
 // create grid on page load
 $('document').ready(function() {
-	createGrid(16);
+	createGrid(16, 16);
 
 	$('.grid').mouseover(function() {
 		$(this).css('background-color', 'blue');
